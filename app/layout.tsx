1a9674e542ad2b1e37403401a5443a9573e85595
gabislera <gabiicarniel@gmail.com>
2025-11-03 18:16:37 -0300

    feat: add header in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex flex-col min-h-screen">
+          <header className="w-full border-b border-[#222729] py-6 px-8">
+            <h1>VAROS</h1>
+          </header>
+          {children}
+        </div>
       </body>
     </html>
   );
